dd4f4d68756f05c3ff96b49ab7c5cffddcfacd29
Yujin <ykim.code@gmail.com>
2024-08-08 20:44:01 -0400

    solving pdf scale problem

--- resume.js
+++ resume.js
@@ -6,21 +6,21 @@ import * as pdf from "https://unpkg.com/pdfjs-dist@4.5.136/build/pdf.min.mjs";
 // pdf.GlobalWorkerOptions.workerSrc = pdfjsWorker;
 pdf.GlobalWorkerOptions.workerSrc = "https://unpkg.com/pdfjs-dist@4.5.136/build/pdf.worker.mjs";
 
-pdf.getDocument("http://127.0.0.1:3000/Resume.pdf");
-
 const resume = await pdf.getDocument("./Resume.pdf").promise;
 console.log(resume);
 
 const page = await resume.getPage(1);
 const scale = 1.5;
-const viewport = page.getViewport(scale);
+const viewport = page.getViewport({ scale: scale });
 const canvas = document.getElementById("resumeCanvas");
 const context = canvas.getContext("2d");
-// canvas.height = viewport.height;
-// canvas.width = viewport.width;
+canvas.height = viewport.height;
+canvas.width = viewport.width;
 const renderContext = {
     canvasContext: context,
     viewport: viewport,
 };
 
-page.render(renderContext);
+page.render(renderContext).promise.then(function () {
+    console.log("Page rendered!");
+});
